1ae61624f2dc48c8a995f01ff9728bc04b990826
Iyxan23 <nurihsanalghifari@gmail.com>
2023-09-21 13:31:56 +0700

    basic-7644: some consistency changes

--- weekly-7644/basic/test.js
+++ weekly-7644/basic/test.js
@@ -1,4 +1,4 @@
-const lib = require("./lib")
+const { camelCaseify } = require("./lib")
 
 // test given from the official smkdev page
 // - underscore_case
@@ -8,21 +8,21 @@ const lib = require("./lib")
 // - delayed_departure
 
 test("convert underscore_case to underscoreCase", () => {
-  expect(lib.camelCaseify("underscore_case")).toBe("underscoreCase")
+  expect(camelCaseify("underscore_case")).toBe("underscoreCase")
 });
 
 test("convert first_name to firstName", () => {
-  expect(lib.camelCaseify("first_name")).toBe("firstName")
+  expect(camelCaseify("first_name")).toBe("firstName")
 });
 
 test("convert Some_Variable to someVariable", () => {
-  expect(lib.camelCaseify("Some_Variable")).toBe("someVariable")
+  expect(camelCaseify("Some_Variable")).toBe("someVariable")
 });
 
 test("convert calculate_AGE to calculateAge", () => {
-  expect(lib.camelCaseify("calculate_AGE")).toBe("calculateAge")
+  expect(camelCaseify("calculate_AGE")).toBe("calculateAge")
 });
 
 test("convert delayed_departure to delayedDeparture", () => {
-  expect(lib.camelCaseify("delayed_departure")).toBe("delayedDeparture")
+  expect(camelCaseify("delayed_departure")).toBe("delayedDeparture")
 });
